6173cf1b960935227aa77beab664a90dbb566e8d
DonMB77 <purposefuldrifter@gmail.com>
2025-10-16 15:17:11 +0200

    main and screen setup

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+def main ():
+    screen = Screen()
+    screen.setup(800,600)
+    screen.bgcolor("black")
+    screen.title("Pong Game!")
+    screen.exitonclick()
+    return 0
+
+if __name__ == '__main__':
+    main()
