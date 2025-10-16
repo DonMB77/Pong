3324f8a0887beb566bb340479c3623f5e120fadd
DonMB77 <purposefuldrifter@gmail.com>
2025-10-16 17:00:34 +0200

    key listener and game loop

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ def main ():
     screen.setup(800,600)
     screen.bgcolor("black")
     screen.title("Pong Game!")
-    paddle = Paddle(350, 0)
-    screen.exitonclick()
+    paddle_r = Paddle(350, 0)
+    paddle_l = Paddle(-350, 0)
+    screen.listen()
+    screen.onkey(paddle_r.go_up, "Up")
+    screen.onkey(paddle_r.go_down, "Down")
+    screen.onkey(paddle_l.go_up, "w")
+    screen.onkey(paddle_l.go_down, "s")
+    is_game_on = True
+    while is_game_on:
+        screen.update()
+    is_game_on = True
     return 0
 
 if __name__ == '__main__':
--- paddle.py
+++ paddle.py
@@ -11,3 +11,11 @@ class Paddle:
         self.paddle.penup()
         self.paddle.goto(x_pos, y_pos)
         self.paddle.showturtle()
+
+    def go_up(self):
+        new_y = self.paddle.ycor() + 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.paddle.ycor() - 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
